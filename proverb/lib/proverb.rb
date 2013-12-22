31f15e582d18453da820c11beacbef1d3b5e156e
Will Mitchell <wvmitchell@gmail.com>
2013-12-21 17:52:55 -0700

    Changed things to items

--- proverb/lib/proverb.rb
+++ proverb/lib/proverb.rb
@@ -1,24 +1,25 @@
 class Proverb
 
-  def initialize(*things)
-    @qualifier = things[-1][:qualifier] if things[-1].class == Hash
-    @things = @qualifier ? things[0..-2] : things
-    @lines = @things.count - 1
+  def initialize(*items)
+    @qualifier = items[-1][:qualifier] if items[-1].class == Hash
+    @items = @qualifier ? items[0..-2] : items
   end
 
   def to_s
     stanza = ""
-    @things.each_cons(2) do |pair|
+    @items.each_cons(2) do |pair|
       stanza += "For want of a #{pair[0]} the #{pair[1]} was lost.\n"
     end
     stanza += final_line
   end
 
+  private
+
   def final_line
     if @qualifier
-      "And all for the want of a #{@qualifier + ' ' + @things[0]}."
+      "And all for the want of a #{@qualifier + ' ' + @items[0]}."
     else
-      "And all for the want of a #{@things[0]}."
+      "And all for the want of a #{@items[0]}."
     end
   end
 
