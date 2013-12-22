d9be97ce17225ae3ca77377de2ee6e065ae5ab9d
Will Mitchell <wvmitchell@gmail.com>
2013-12-21 17:50:20 -0700

    Reworked proverb implementation to avoid recursive function

--- proverb/lib/proverb.rb
+++ proverb/lib/proverb.rb
@@ -1,43 +1,24 @@
 class Proverb
 
-  attr_reader :items
-
-  def initialize(*items)
-    @qualifier = find_qualifier(items[-1])
-    create_proverb_list(items)
+  def initialize(*things)
+    @qualifier = things[-1][:qualifier] if things[-1].class == Hash
+    @things = @qualifier ? things[0..-2] : things
+    @lines = @things.count - 1
   end
 
   def to_s
-    phrase(items)
-  end
-
-  private
-
-  def find_qualifier(option)
-    @qualifier = option.class == Hash ? option[:qualifier] + ' ' : ''
-  end
-
-  def create_proverb_list(items)
-    @qualifier ? @items = items[0..-2] : @items = items
-  end
-
-  def phrase(item_list)
-    item_list.length > 1 ? proverb_lines(item_list) : final_line
-  end
-
-  def proverb_lines(item_list)
-    single_line(item_list) + phrase(item_list[1..-1])
-  end
-
-  def single_line(items)
-    "For want of a #{items[0]} the #{items[1]} was lost.\n"
+    stanza = ""
+    @things.each_cons(2) do |pair|
+      stanza += "For want of a #{pair[0]} the #{pair[1]} was lost.\n"
+    end
+    stanza += final_line
   end
 
   def final_line
     if @qualifier
-      "And all for the want of a #{@qualifier + items[0]}."
+      "And all for the want of a #{@qualifier + ' ' + @things[0]}."
     else
-      "And all for the want of a #{items[0]}."
+      "And all for the want of a #{@things[0]}."
     end
   end
 
