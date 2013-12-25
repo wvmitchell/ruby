9973e42d96d217818489ff62cb72c0447ebc763b
Will Mitchell <wvmitchell@gmail.com>
2013-12-25 16:43:11 -0500

    Changed solution to use metaprogramming

--- space-age/lib/space_age.rb
+++ space-age/lib/space_age.rb
@@ -4,81 +4,22 @@ class SpaceAge
 
   def initialize(seconds)
     @age_in_seconds = seconds
+    planet_seconds.each do |planet, seconds|
+      self.class.send :define_method, "on_#{planet}" , lambda { age(seconds)}
+    end
   end
 
   def seconds
     age_in_seconds
   end
 
-  def earth_year
-    31557600
-  end
-
-  def mercury_year
-    7600526
-  end
-
-  def venus_year
-    19411026
-  end
-
-  def mars_year
-    59359777
-  end
-
-  def jupiter_year
-    374222565
-  end
-
-  def saturn_year
-    928792570
-  end
-
-  def uranus_year
-    2652994592
-  end
-
-  def neptune_year
-    5196280668
-  end
-
-  def on_earth
-    age(earth_year)
-  end
-
-  def on_mercury
-    age(mercury_year)
-  end
-
-  def on_venus
-    age(venus_year)
-  end
-
-  def on_mars
-    age(mars_year)
-  end
-
-  def on_jupiter
-    age(jupiter_year)
-  end
-
-  def on_saturn
-    age(saturn_year)
-  end
-
-  def on_uranus
-    age(uranus_year)
-  end
-
-  def on_neptune
-    age(neptune_year)
+  def planet_seconds
+    {mercury: 7600526, venus: 19411026, earth: 31557600, mars: 59359777, jupiter: 374222565,
+     saturn: 928792570, uranus: 2652994592, neptune: 5196280668}
   end
 
   def age(planet_year_seconds)
     (age_in_seconds.to_f / planet_year_seconds.to_f).round(2)
   end
 
-  def method_missing(name, *args)
-    puts name
-  end
 end
