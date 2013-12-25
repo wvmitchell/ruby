331eb680eef16a4da3430b437c9f0d16aeb8fb84
Will Mitchell <wvmitchell@gmail.com>
2013-12-25 14:46:27 -0500

    First working implementation of space age

--- space-age/lib/space_age.rb
+++ space-age/lib/space_age.rb
@@ -10,15 +10,75 @@ class SpaceAge
     age_in_seconds
   end
 
-  def on_earth
-    (age_in_seconds.to_f / seconds_per_earth_year.to_f).round(2)
-  end
-
-  def seconds_per_earth_year
+  def earth_year
     31557600
   end
 
+  def mercury_year
+    7600526
+  end
+
+  def venus_year
+    19411026
+  end
+
+  def mars_year
+    59359777
+  end
+
+  def jupiter_year
+    374222565
+  end
+
+  def saturn_year
+    928792570
+  end
+
+  def uranus_year
+    2652994592
+  end
+
+  def neptune_year
+    5196280668
+  end
+
+  def on_earth
+    age(earth_year)
+  end
+
   def on_mercury
-    280.88
+    age(mercury_year)
+  end
+
+  def on_venus
+    age(venus_year)
+  end
+
+  def on_mars
+    age(mars_year)
+  end
+
+  def on_jupiter
+    age(jupiter_year)
+  end
+
+  def on_saturn
+    age(saturn_year)
+  end
+
+  def on_uranus
+    age(uranus_year)
+  end
+
+  def on_neptune
+    age(neptune_year)
+  end
+
+  def age(planet_year_seconds)
+    (age_in_seconds.to_f / planet_year_seconds.to_f).round(2)
+  end
+
+  def method_missing(name, *args)
+    puts name
   end
 end
