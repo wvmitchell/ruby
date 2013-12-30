361b12479ee01609be81cd703e63fb4045eb6554
Will Mitchell <wvmitchell@gmail.com>
2013-12-30 11:14:58 -0500

    First implementation finished

--- secret_handshake/lib/secret_handshake.rb
+++ secret_handshake/lib/secret_handshake.rb
@@ -1,10 +1,26 @@
 class SecretHandshake
 
-  def initialize(arg)
+  def initialize(code)
+    code = 0 if code.class != Fixnum
+    @code = code.to_s(2).split('').reverse.collect(&:to_i)
   end
 
   def commands
-    ['wink']
+    reverse_if_needed(create_handshake)
+  end
+
+  def signs
+    ['wink', 'double blink', 'close your eyes', 'jump']
+  end
+
+  def reverse_if_needed(handshake)
+    @code[4] == 1 ? handshake.reverse : handshake
+  end
+
+  def create_handshake
+    (0..3).each_with_object([]) do |i, handshake|
+      handshake << signs[i] if @code[i] == 1
+    end
   end
 
 end
