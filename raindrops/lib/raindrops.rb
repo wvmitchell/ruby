3470c941d8c81fbca0cf9de6d576dcf4b6d0a9be
Will Mitchell <wvmitchell@gmail.com>
2014-01-25 11:47:50 -0700

    Working raindrops implementation v1

--- raindrops/lib/raindrops.rb
+++ raindrops/lib/raindrops.rb
@@ -1,14 +1,29 @@
+require 'prime'
+
 class Raindrops
 
   def convert(drops)
-    case drops
-    when 1
-      '1'
-    when 3
-      'Pling'
-    else
-      'Plong'
+    small_primes = find_small_prime_factors(drops)
+    small_primes.empty? ? drops.to_s : generate_string(small_primes)
+  end
+
+  def find_small_prime_factors(drops)
+    Prime.prime_division(drops).flatten.uniq.reject do |value|
+      ![3,5,7].include? value
     end
+  end
+
+  def generate_string(primes)
+    primes.collect do |prime|
+      case prime
+      when 3
+        "Pling"
+      when 5
+        "Plang"
+      when 7
+        "Plong"
+      end
+    end.join
   end
 
 end
