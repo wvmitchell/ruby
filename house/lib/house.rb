81478a9080dc0622fbe3fbb20cfbdb8f94a7ebf8
Will Mitchell <wvmitchell@gmail.com>
2014-02-10 08:31:16 -0700

    first two tests passing

---
+++ house/lib/house.rb
@@ -0,0 +1,12 @@
+class House
+
+  def verse(lines)
+    case lines
+    when 1
+      "This is the house that Jack built.\n"
+    when 2
+      "This is the malt that lay in the house that Jack built.\n"
+    end
+  end
+
+end
