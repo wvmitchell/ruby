008d1437756f72484586f9b0155b009d586d853d
William Mitchell <wvmitchell@gmail.com>
2014-02-14 10:22:41 -0700

    all but final test passing

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,12 +1,25 @@
 class House
 
   def verse(lines)
-    case lines
-    when 1
-      "This is the house that Jack built.\n"
-    when 2
-      "This is the malt that lay in the house that Jack built.\n"
-    end
+    start = "This is the "
+    finish = "house that Jack built.\n"
+    start + middle.take(lines-1).reverse.join + finish
+  end
+
+  def middle
+    [
+      'malt that lay in the ',
+      'rat that ate the ',
+      'cat that killed the ',
+      'dog that worried the ',
+      'cow with the crumpled horn that tossed the ',
+      'maiden all forlorn that milked the ',
+      'man all tattered and torn that kissed the ',
+      'priest all shaven and shorn that married the ',
+      'rooster that crowed in the morn that woke the ',
+      'farmer sowing his corn that kept the ',
+      'horse and the hound and the horn that belonged to the '
+    ]
   end
 
 end
