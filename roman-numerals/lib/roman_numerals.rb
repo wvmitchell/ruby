df084a82cd083c3b087cd4a74ebd22c814880d9f
Will Mitchell <wvmitchell@gmail.com>
2014-03-01 12:31:21 -0700

    working implementation, needs refactor

--- roman-numerals/lib/roman_numerals.rb
+++ roman-numerals/lib/roman_numerals.rb
@@ -7,38 +7,63 @@ class Fixnum
         letters << 'IV'
       elsif nine?
         letters << 'IX'
-      else
-        letters << 'XL' + (self%40).to_roman
+      elsif forties?
+        letters << 'XL' + (self-40).to_roman
+      elsif nineties?
+        letters << 'XC' + (self-90).to_roman
+      elsif fourhundreds?
+        letters << 'CD' + (self-400).to_roman
+      elsif ninehundreds?
+        letters << 'CM' + (self-900).to_roman
       end
     else
-      tens = self / 10
-      test = self % 10
-      fives = test / 5
-      test = test % 5
-      ones = test / 1
-      add_tens(letters, tens)
-      add_fives(letters, fives)
-      add_ones(letters, ones)
+      if thousand_or_greater?
+        letters << 'M' + (self-1000).to_roman
+      elsif fivehundred_or_greater?
+        letters << 'D' + (self-500).to_roman
+      elsif onehundred_or_greater?
+        letters << 'C' + (self-100).to_roman
+      elsif fifty_or_greater?
+        letters << 'L' + (self-50).to_roman
+      elsif ten_or_greater?
+        letters << 'X' + (self-10).to_roman
+      elsif five_or_greater?
+        letters << 'V' + (self-5).to_roman
+      else
+        letters << 'I' * self
+      end
     end
     letters.join
   end
 
   def edge_case
-    four? || nine? || forties?
+    four? || nine? || forties? || nineties? || fourhundreds? || ninehundreds?
   end
 
   private
 
-  def add_tens(letters, num)
-    num.times { letters << 'X' }
+  def thousand_or_greater?
+    self >= 1000
   end
 
-  def add_fives(letters, num)
-    num.times { letters << 'V'}
+  def fivehundred_or_greater?
+    self >= 500
   end
 
-  def add_ones(letters, num)
-    num.times { letters << 'I' }
+  def onehundred_or_greater?
+    self >= 100
+  end
+
+  def fifty_or_greater?
+    self >= 50
+  end
+
+  def ten_or_greater?
+    self >= 10
+  end
+
+  def five_or_greater?
+    self >= 5
   end
 
   def four?
@@ -51,6 +76,18 @@ class Fixnum
 
   def forties?
     self >= 40 && self < 50
+  end
+
+  def nineties?
+    self >= 90 && self < 100
+  end
+
+  def fourhundreds?
+    self >= 400 && self < 500
+  end
+
+  def ninehundreds?
+    self >= 900 && self < 1000
   end
 
 end
--- roman-numerals/test/roman_numerals_test.rb
+++ roman-numerals/test/roman_numerals_test.rb
@@ -46,42 +46,34 @@ class RomanTest < MiniTest::Unit::TestCase
   end
 
   def test_93
-    skip
     assert_equal 'XCIII', 93.to_roman
   end
 
   def test_141
-    skip
     assert_equal 'CXLI', 141.to_roman
   end
 
   def test_163
-    skip
     assert_equal 'CLXIII', 163.to_roman
   end
 
   def test_402
-    skip
     assert_equal 'CDII', 402.to_roman
   end
 
   def test_575
-    skip
     assert_equal 'DLXXV', 575.to_roman
   end
 
   def test_911
-    skip
     assert_equal 'CMXI', 911.to_roman
   end
 
   def test_1024
-    skip
     assert_equal 'MXXIV', 1024.to_roman
   end
 
   def test_3000
-    skip
     assert_equal 'MMM', 3000.to_roman
   end
 
