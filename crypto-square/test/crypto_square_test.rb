f7ff4dba60c31017f043fff15286bcf984646ea7
Will Mitchell <wvmitchell@gmail.com>
2014-03-07 15:58:38 -0700

    seven tests passing

--- crypto-square/test/crypto_square_test.rb
+++ crypto-square/test/crypto_square_test.rb
@@ -34,7 +34,6 @@ class CryptoTest < MiniTest::Unit::TestCase
   end
 
   def test_other_plaintext_segments
-    skip
     crypto = Crypto.new('ZOMG! ZOMBIES!!!')
     assert_equal ["zomg", "zomb", "ies"], crypto.plaintext_segments
   end
