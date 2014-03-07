be3a14e3431393998e12afa0ee4016807cbfe228
Will Mitchell <wvmitchell@gmail.com>
2014-03-07 15:12:56 -0700

    four tests passing

--- matrix/test/matrix_test.rb
+++ matrix/test/matrix_test.rb
@@ -20,7 +20,6 @@ class MatrixTest < MiniTest::Unit::TestCase
   end
 
   def test_extract_other_row_again
-    skip
     matrix = Matrix.new("1 4 9\n16 25 36")
     assert_equal [16, 25, 36], matrix.rows[1]
   end
