a31073118d89c202b68d4fe41b52592e200bd1fa
William Mitchell <wvmitchell@gmail.com>
2014-03-28 10:59:10 -0600

    all tests passing, needs refactor

--- pig-latin/lib/pig_latin.rb
+++ pig-latin/lib/pig_latin.rb
@@ -1,11 +1,28 @@
 class PigLatin
 
   def self.translate(phrase)
-    if phrase[0].vowel?
-      phrase + "ay"
+    words = phrase.split(" ")
+    words.collect {|word| interpret(word)}.join(" ")
+  end
+
+  private
+
+  def self.interpret(word)
+    if word[0..1] == "ye"
+      rotation(1, word)
+    elsif word[0].vowel? || (word[0] == 'x' && !word[1].vowel?)
+      word + "ay"
+    elsif %w(squ thr sch).include? word[0..2]
+      rotation(3, word)
+    elsif %w(ch qu th).include? word[0..1]
+      rotation(2, word)
     else
-      phrase.split("").rotate.join + "ay"
+      rotation(1, word)
     end
+  end
+
+  def self.rotation(num, word)
+    word.split("").rotate(num).join + "ay"
   end
 
 end
--- pig-latin/test/pig_latin_test.rb
+++ pig-latin/test/pig_latin_test.rb
@@ -25,52 +25,42 @@ class PigLatinTest < MiniTest::Unit::TestCase
   end
 
   def test_word_beginning_with_qu
-    skip
     assert_equal "eenquay", PigLatin.translate("queen")
   end
 
   def test_word_with_consonant_preceding_qu
-    skip
     assert_equal "aresquay", PigLatin.translate("square")
   end
 
   def test_word_beginning_with_th
-    skip
     assert_equal "erapythay", PigLatin.translate("therapy")
   end
 
   def test_word_beginning_with_thr
-    skip
     assert_equal "ushthray", PigLatin.translate("thrush")
   end
 
   def test_word_beginning_with_sch
-    skip
     assert_equal "oolschay", PigLatin.translate("school")
   end
 
   def test_translates_phrase
-    skip
     assert_equal "ickquay astfay unray", PigLatin.translate("quick fast run")
   end
 
   def test_word_beginning_with_ye
-    skip
     assert_equal "ellowyay", PigLatin.translate("yellow")
   end
 
   def test_word_beginning_with_yt
-    skip
     assert_equal "yttriaay", PigLatin.translate("yttria")
   end
 
   def test_word_beginning_with_xe
-    skip
     assert_equal "enonxay", PigLatin.translate("xenon")
   end
 
   def test_word_beginning_with_xr
-    skip
     assert_equal "xrayay", PigLatin.translate("xray")
   end
 end
