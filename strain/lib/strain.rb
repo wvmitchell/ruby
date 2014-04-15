9e0916ec94dafa5cc2176d50f9363dd5cba072a8
Will Mitchell <wvmitchell@gmail.com>
2014-04-15 16:17:21 -0600

    made use of collect rather than each

--- strain/lib/strain.rb
+++ strain/lib/strain.rb
@@ -1,19 +1,15 @@
 class Array
 
   def keep(&block)
-    collection = []
-    self.each do |elem|
-      collection << elem if block.call(elem)
+    self.collect do |elem|
+      elem if block.call(elem)
     end
-    collection
   end
 
   def discard(&block)
-    collection = []
-    self.each do |elem|
-      collection << elem unless block.call(elem)
+    self.collect do |elem|
+      elem unless block.call(elem)
     end
-    collection
   end
 
 end
