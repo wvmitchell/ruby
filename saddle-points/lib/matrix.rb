8eb1f02008f24dfd9d3fcdc221c022e16c29626f
Will Mitchell <wvmitchell@gmail.com>
2014-04-18 12:24:04 -0600

    all tests passing

--- saddle-points/lib/matrix.rb
+++ saddle-points/lib/matrix.rb
@@ -1,15 +1,27 @@
 class Matrix
 
+  attr_reader :matrix
+
   def initialize(matrix_string)
     @matrix = parse(matrix_string)
   end
 
   def rows
-    @matrix
+    matrix
   end
 
   def columns
     rows.transpose
+  end
+
+  def saddle_points
+    points = []
+    rows.each_index do |r|
+      columns.each_index do |c|
+        points << [r, c] if saddle_point?(r, c)
+      end
+    end
+    points
   end
 
   private
@@ -18,6 +30,13 @@ class Matrix
     matrix_string.split("\n").collect do |row|
       row.split.collect(&:to_i)
     end
+  end
+
+  def saddle_point?(x, y)
+    row = rows[x]
+    column = columns[y]
+    point = matrix[x][y]
+    row.max == point && column.min == point
   end
 
 end
