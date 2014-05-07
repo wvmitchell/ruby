74e25f5d74da79a9415ae6adc5b04d0d8b3eff82
Will Mitchell <wvmitchell@gmail.com>
2014-05-07 11:05:46 -0600

    three tests passing

--- sum-of-multiples/lib/sum_of_multiples.rb
+++ sum-of-multiples/lib/sum_of_multiples.rb
@@ -2,10 +2,10 @@ class SumOfMultiples
 
   def self.to(up_to)
     (0...up_to).inject do |sum, num|
-      if num % 3 == 0
+      if num % 3 == 0 || num % 5 == 0
         sum + num
       else
-        sum + 0
+        sum
       end
     end
   end
