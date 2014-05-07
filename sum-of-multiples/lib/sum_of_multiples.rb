5f779660ad2f0a32b040855997ec3514ca7744fa
Will Mitchell <wvmitchell@gmail.com>
2014-05-07 11:11:02 -0600

    all tests passing, can sum to arbitrary number with arbitrary multiples

--- sum-of-multiples/lib/sum_of_multiples.rb
+++ sum-of-multiples/lib/sum_of_multiples.rb
@@ -1,8 +1,24 @@
 class SumOfMultiples
 
+  attr_reader :multiples
+
+  def initialize(*multiples)
+    @multiples = multiples
+  end
+
   def self.to(up_to)
     (0...up_to).inject do |sum, num|
       if num % 3 == 0 || num % 5 == 0
+        sum + num
+      else
+        sum
+      end
+    end
+  end
+
+  def to(up_to)
+    (0...up_to).inject do |sum, num|
+      if multiples.any? {|mult| num % mult == 0}
         sum + num
       else
         sum
