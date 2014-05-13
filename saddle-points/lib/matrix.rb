e20aa21b733ee53b6ecb8b3836e9c18ad3ab1e5a
Will Mitchell <wvmitchell@gmail.com>
2014-05-13 13:17:07 -0600

    all tests passing

--- saddle-points/lib/matrix.rb
+++ saddle-points/lib/matrix.rb
@@ -2,17 +2,39 @@ class Matrix
 
   attr_reader :rows, :columns
 
-  def initialize(matrix)
-    @rows = parse(matrix)
+  def initialize matrix
+    @rows = parse matrix
     @columns = rows.transpose
+  end
+
+  def saddle_points
+    positions.select do |position|
+      is_saddle_point? position
+    end
   end
 
   private
 
-  def parse(matrix)
+  def is_saddle_point?(position)
+    value = rows[position[0]][position[1]]
+    rows[position[0]].all? {|test| value >= test} &&
+    columns[position[1]].all? {|test| value <= test}
+  end
+
+  def parse matrix
     matrix.lines.collect do |row|
-      row.split.collect(&:to_i)
+      row.split.collect &:to_i
     end
+  end
+
+  def positions
+    positions = []
+    (0...(rows.count)).each do |row|
+      (0...(columns.count)).each do |col|
+        positions << [row,col]
+      end
+    end
+    positions
   end
 
 end
