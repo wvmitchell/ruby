32e4dfd2955f6b6b2964125910ffb2780d29826c
Will Mitchell <wvmitchell@gmail.com>
2014-05-16 13:03:00 -0600

    all tests now passing

--- octal/lib/octal.rb
+++ octal/lib/octal.rb
@@ -4,10 +4,17 @@ class Octal
 
   def initialize(octaldecimal)
     @octaldecimal = octaldecimal
+    check_invalid
   end
 
   def to_decimal
     octaldecimal.to_i(8)
+  end
+
+  def check_invalid
+    if octaldecimal.chars.any? {|char| char.to_i > 7}
+      @octaldecimal = "0"
+    end
   end
 
 end
