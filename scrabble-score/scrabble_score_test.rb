03ebe7fdd2a5cf26b4471ac31f320a8b5e37bab1
Will Mitchell <wvmitchell@gmail.com>
2014-12-01 13:58:16 -0700

    fifth test passing

--- scrabble-score/scrabble_score_test.rb
+++ scrabble-score/scrabble_score_test.rb
@@ -21,12 +21,10 @@ class ScrabbleTest < MiniTest::Unit::TestCase
   end
 
   def test_scores_other_very_short_word
-    skip
     assert_equal 4, Scrabble.new('f').score
   end
 
   def test_simple_word_scores_the_number_of_letters
-    skip
     assert_equal 6, Scrabble.new("street").score
   end
 
